a21603e2cd8a1bb0a4697a54df5d2b45cfb1d8b5
Wasiu Idowu <hoxtygen@live.com>
2019-12-11 00:15:34 +0100

    Add redux store

--- client/library/src/App.js
+++ client/library/src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Context from './context/Contex'
-import HomePage from './views/HomePage';
 
 function App() {
-  const user = {
-    name: 'McDowell',
-    age: '54',
-    loggedIn: true
-  }
   return (
-    <Context.Provider value = {user}>
     <div className="App">
-     <HomePage />
+     <h1>Hello, world. This is a library of books</h1>
     </div>
-    </Context.Provider>
   );
 }
 
