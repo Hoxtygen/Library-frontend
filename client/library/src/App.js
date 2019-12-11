51374c6177b8d1384e06251dd78e64c652fcf7c1
Wasiu Idowu <hoxtygen@live.com>
2019-12-11 18:29:12 +0100

    Change from CSS to SCSS

--- client/library/src/App.js
+++ client/library/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
+import './styles/App.scss';
 import Home from './container/Home';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello, world. This is a library of books</h1>
      <Home />
     </div>
   );
