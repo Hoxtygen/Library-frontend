ae221ecfe7bd7340b5b270e20f027f8b231182de
Wasiu Idowu <hoxtygen@live.com>
2020-01-09 14:31:13 +0100

    Add subcomponents

--- client/library/src/App.js
+++ client/library/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import './styles/App.scss';
+import './styles/App.css';
 import Home from './container/Home';
+import Aside from './component/Aside';
+import Header from './component/Header';
 
 function App() {
   return (
     <div className="App">
+    <Aside />
+    <main>
+    <Header />
      <Home />
+    </main>
     </div>
   );
 }
