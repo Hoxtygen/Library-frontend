632f4ec61952655104430763dbedaaa9341ac0c5
Wasiu Idowu <hoxtygen@live.com>
2020-01-09 14:21:34 +0100

    Remove header element

--- client/library/src/component/Aside.js
+++ client/library/src/component/Aside.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import Categories from '../container/Categories';
 
 export default function Aside() {
     return (
         <aside>
-            <h1>I am the sidebar</h1>
+            <div className="logo">
+            <h1>Bookr</h1>
+            </div>
+            <div className="sidebar-content">
+                <div className="btn-container">
+                    <button type="button">Add book</button>
+                </div>
+                <div className="fav-authors">
+                    <h3 className="fav-title">Favourite Authors</h3>
+                    <ul>
+                        <li><a href="/">John Grisham</a></li>
+                        <li><a href="/">Robin Cook</a></li>
+                        <li><a href="/">Sir Arthor Conan Doyle</a></li>
+                        <li><a href="/">Jeffery Deaver</a></li>
+                        <li><a href="/">Michael Connelly</a></li>
+                    </ul>
+                </div>
+                <div className="category-container">
+                    <h3 className = "category-title">Categories</h3>
+                    <Categories />
+                </div>
+               
+            </div>
         </aside>
     )
 }
