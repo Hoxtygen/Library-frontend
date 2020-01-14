26efb391fb142d955cd37ac84fd5887f42fd6999
Wasiu Idowu <hoxtygen@live.com>
2020-01-14 13:06:16 +0100

    Add custom hooks for dropdown

---
+++ client/library/src/component/useDropdown.js
@@ -0,0 +1,28 @@
+import React, { Fragment, useState } from "react";
+
+const useDropdown = (label, defaultState, options) => {
+  const [state, setState] = useState(defaultState);
+  const id = `use-dropdown-${label.replace(" ", "").toLowerCase()}`;
+  const Dropdown = () => (
+    <Fragment>
+      <label htmlFor={id}>{label}</label>
+      <select
+        value={state}
+        id={id}
+        onChange={e => setState(e.target.value)}
+        onBlur={e => setState(e.target.value)}
+        disabled={options.length === 0}
+      >
+        <option>All</option>
+        {options.map(item => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+    </Fragment>
+  );
+  return [state, Dropdown, setState];
+};
+
+export default useDropdown;
